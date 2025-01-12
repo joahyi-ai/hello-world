42996e0d0cbd12b2582755eae4697aff71c2eab3
joahyi-ai <joahyi7@gmail.com>
2025-01-12 11:21:50 +0800

    first commit

--- src/content.js
+++ src/content.js
@@ -1,13 +1,13 @@
-import * as InboxSDK from '@inboxsdk/core';
+import * as InboxSDK from "@inboxsdk/core";
 
-InboxSDK.load(2, "Hello World!").then((sdk) => {
+InboxSDK.load(2, "sdk_pro111_72e70e643e").then((sdk) => {
   // the SDK has been loaded, now do something with it!
   sdk.Compose.registerComposeViewHandler((composeView) => {
     // a compose view has come into existence, do something with it!
     composeView.addButton({
       title: "My Nifty Button!",
       iconUrl:
-        "https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365",
+        "https://img.icons8.com/?size=100&id=1349&format=png&color=000000",
       onClick(event) {
         event.composeView.insertTextIntoBodyAtCursor("Hello World!");
       },
